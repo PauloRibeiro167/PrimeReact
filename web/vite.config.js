1f5a3faa7f3a7bfacf6d8425c395acccc42af637
PauloRibeiro167 <146147732+PauloRibeiro167@users.noreply.github.com>
2024-10-17 12:39:39 +0000

    feat: criação de logica para imports de styles dentro do vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@admin-styles': path.resolve(__dirname, 'src/assets/stylesheet/Admin'),
-      '@app-styles': path.resolve(__dirname, 'src/assets/stylesheet/App'),
-      '@public-styles': path.resolve(__dirname, 'src/assets/stylesheet/Public'),
+      '@admin-styles': path.resolve(__dirname, 'src/assets/stylesheet/admin'),
+      '@public-styles': path.resolve(__dirname, 'src/assets/stylesheet/public'),
+      '@pwa-styles': path.resolve(__dirname, 'src/app/assets/stylesheets/pwa'),
+      '@layouts': path.resolve(__dirname, 'src/app/assets/stylesheet/layouts'),
     },
   },
 });
